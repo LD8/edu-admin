e887e0d64afce731814af35cc4d39e3ce23f1849
ld8 <don_lee@me.com>
2021-01-17 23:28:47 +0800

    init layout and app aside

---
+++ src/layout/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container>
+    <el-aside width="200px"><app-aside /></el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <!-- 子路由出口 -->
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import AppAside from './components/app-aside'
+
+export default Vue.extend({
+  name: 'Layout',
+  components: {
+    AppAside
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.el-container {
+  min-height: 100vh;
+  min-width: 980px;
+}
+.el-aside {
+  background: rgb(227, 227, 227);
+}
+.el-header {
+  background: rgb(182, 182, 182);
+}
+.el-main {
+  background: rgb(233, 233, 233);
+}
+</style>
---
+++ src/views/error-page/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>404</div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Error404'
+})
+</script>
+
+<style scoped>
+</style>
