0e3fa0aa457a5dd621c196469957f9f56b7b4a44
ld8 <don_lee@me.com>
2021-01-17 22:02:03 +0800

    config eslint and add scss files and elementUI

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,32 @@ module.exports = {
   env: {
     node: true
   },
+  // 使用插件的编码规则
   extends: [
+    // "eslint-plugin-vue": "^6.2.2"：效验 vue 项目中的 vue 文件：https://eslint.vuejs.org/
     'plugin:vue/essential',
+    // "@vue/eslint-config-standard": "^5.1.2"：standard 效验规则（行尾不应有分号）
     '@vue/standard',
+    // "@vue/eslint-config-typescript": "^5.0.2"：通过 ESLint 效验TS：https://github.com/vuejs/eslint-config-typescript#readme
     '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
+  // 自定义编码效验规则：https://cn.eslint.org/docs/rules
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: 1,
+    // 配置 TS 效验规则：查询文档 https://github.com/vuejs/eslint-config-typescript#readme
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        }
+      }
+    ]
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+// 下面全局样式中已经加载过，这里不用再加载 elementUI的样式
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// 加载全局样式
+import './styles/index.scss'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
