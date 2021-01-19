9e280ae296a72d3769bb1ea94bc69f57583bfa6e
lipeiwen <lipeiwen@chaintor.com>
2021-01-19 17:26:07 +0800

    feat: user list done

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>广告管理</div>
+  <el-card>
+    <div slot="header">
+      <span>用户管理</span>
+    </div>
+    <user-list />
+  </el-card>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
+import UserList from "./components/UserList.vue";
 
 export default Vue.extend({
-  name: 'AdvertIndex'
-})
+  name: "UserIndex",
+  components: {
+    UserList
+  }
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
